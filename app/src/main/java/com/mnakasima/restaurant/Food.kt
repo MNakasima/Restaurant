9d8ecf54ab943a9093c76cbbf0670dfd62a451ab
Maruko <marco.k95@hotmail.com>
2019-11-11 15:34:44 -0300

    gridview layout

---
+++ app/src/main/java/com/mnakasima/restaurant/Food.kt
@@ -0,0 +1,15 @@
+package com.mnakasima.restaurant
+
+class Food {
+
+    var name:String?=null
+    var description:String?=null
+    var image:Int?=null
+
+    constructor(name:String, description:String, image:Int){
+        this.name=name
+        this.description=description
+        this.image=image
+    }
+
+}
